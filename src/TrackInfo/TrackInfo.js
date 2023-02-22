4a18fd6ef6ae913fb8a9b7ac832c97199dbe0de3
Junior <iamjunioru@gmail.com>
2023-02-22 02:53:58 -0300

    fix seleção de música
    agora a div active anterior desativa quando a música é trocada.

--- src/TrackInfo/TrackInfo.js
+++ src/TrackInfo/TrackInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./TrackInfo.css";
 
 export default function TrackInfo({
@@ -7,24 +7,24 @@ export default function TrackInfo({
   setCurrentSongIdx,
   idx,
 }) {
-  const [activeIndex, setActiveIndex] = useState(null);
+  useEffect(() => {
+    document.getElementsByClassName("div-album")[0].classList.add("active");
+  }, []);
 
   const handleSelectSong = () => {
     setCurrentSongIdx(idx);
-    setActiveIndex(idx);
+    const divs = document.getElementsByClassName("div-album");
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].classList.remove("active");
+    }
+    document.getElementsByClassName("div-album")[idx].classList.add("active");
   };
 
   return (
-    <div
-      className={`div-album ${activeIndex === idx ? "active" : ""}`}
-      onClick={handleSelectSong}
-    >
+    <div className={`div-album`} onClick={handleSelectSong}>
       <h3 className="h3-album">ALBUM</h3>
 
-      <h2
-        className="h2-album"
-        style={{ color, cursor: "pointer" }}
-      >
+      <h2 className="h2-album" style={{ color, cursor: "pointer" }}>
         {name}
       </h2>
     </div>
