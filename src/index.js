d20d2573f6c36c120e3045f0988143261109ade3
Junior <iamjunioru@gmail.com>
2023-02-22 05:27:15 -0300

    changes.
    algumas mudanças foram feitas na interface.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// abrir site
+function closeWelcomeScreen() {
+  const welcomeScreen = document.querySelector('.welcome-screen');
+  welcomeScreen.remove();
+}
+
+function handleStartButtonClick() {
+  closeWelcomeScreen();
+}
+
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", handleStartButtonClick);
+
+setTimeout(closeWelcomeScreen, 100000); // remove a tela de boas-vindas após x seg
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
